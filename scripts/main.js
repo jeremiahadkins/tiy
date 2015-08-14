46bed55ce89b7144becbbe8da02ce40024595340
Jeremiah Adkins <jeremiah.adkins@sm-c17pr7ehg8wp.hillholliday.enterprise.int>
2015-08-14 14:22:44 -0400

    Added smooth scroll and bug fixes

--- scripts/main.js
+++ scripts/main.js
@@ -9,41 +9,30 @@ $(document).ready(function(){
 	
 	// Hide and show menu
 	$('#js-toggle').on('click', function() {
-	  $("nav").toggleClass('is-visible');
+		$("nav").toggleClass('is-visible');
 	});
 
 
 	// when you click on a nav-item, replace #title's content with this nav's content
 	// also hide the menu
-	$('.js-nav-item').on('click', function() {
-	  var content = $(this).text();
+	// animate scroll to href# and find offset -90
+	$('.js-nav-item').on('click', function(e) {
+		e.preventDefault();
 
-	  $('#title').html(content);
-	  
-	  $("nav").removeClass('is-visible');
-	})
+		var content = $(this).text();
+
+		$('#title').html(content);
+
+		$("nav").removeClass('is-visible');
+
+		$('html, body').animate({
+			scrollTop: $($(this).attr('href')).offset().top - 90
+		}, 1000)
+	
+	});
+
+
 	// $('.course-list').click(function(){
 	// 	console.log('clicked')
 	// })
 })
-
-
-
-// The function actually applying the offset
-function offsetAnchor() {
-    if(location.hash.length !== 0) {
-        window.scrollTo(window.scrollX, window.scrollY - 90);
-    }
-}
-
-// This will capture hash changes while on the page
-$(window).on("hashchange", function () {
-    offsetAnchor();
-});
-
-// This is here so that when you enter the page with a hash,
-// it can provide the offset in that case too. Having a timeout
-// seems necessary to allow the browser to jump to the anchor first.
-window.setTimeout(function() {
-    offsetAnchor();
-}, 1); // The delay of 1 is arbitrary and may not always work right (although it did in my testing).
